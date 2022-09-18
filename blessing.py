7a0a5fc9adf4acec605192d75ab2252c5cf37bf5
Raphaël <raphael.duhen@epita.fr>
2022-09-18 22:55:36 +0200

    fix: remove '_bless', fix in-game message format

--- blessing.py
+++ blessing.py
@@ -60,10 +60,9 @@ class BlessConfig:
         self.instance: int = config.get('bless_setup', 'relay_instance')
 
         # Bless
-        bless_types: list[Role] = list(map(lambda role: Role(role), "affinity credit resource damage health shield".split(" ")))
-        roles: list[str] = [config.get('roles', f"{role.value}_bless") for role in bless_types]
-        self.roles: dict[Role, str] = {role: name for (role, name) in zip(bless_types, roles) if not not name}
-
+        roles: list[Role] = list(map(lambda role: Role(role), "affinity credit resource damage health shield".split(" ")))
+        names: list[str] = [config.get('roles', role.value) for role in roles]
+        self.roles: dict[Role, str] = {role: name for (role, name) in zip(roles, names) if not not name}
 
         # Config
         self.name: str = config.get('config', 'my_name')
@@ -93,16 +92,16 @@ class BlessConfig:
 separator: str = "=" * 80
 bless_config = BlessConfig("bless.ini" if len(sys.argv) == 1 else sys.argv[1])
 message: str = f"\n{separator}\n".join([
-    f"```", 
+    "```", 
     f"{bless_config.command()}",
-    f"BLESSING ROLES:\n" + \
-        "\n".join(f"@{name} => {role.name}" for (role, name) in bless_config.roles.items()) + \
-        ("\nShield bless will be delayed by 1 minute" if Role.Shield in bless_config.roles.keys() else ""),
+    f"BLESSING ROLES: " + \
+        " | ".join(f"@{name} => {role.name}" for (role, name) in bless_config.roles.items()) + \
+        (" || Shield bless will be delayed by 1 minute" if Role.Shield in bless_config.roles.keys() else ""),
     "\n".join(
         f"/w {name} Reminder for bless at {bless_config.relay.name} {bless_config.instance} in {bless_config.region.name} region. " + \
             f"Role: {role.name}" for (role, name) in bless_config.roles.items()),
     f"Roll call: @{' @'.join(bless_config.roles.values())} :clem:",
-    f"Thanks to {', '.join(list(bless_config.roles.values())[:-1])} and {list(bless_config.roles.values())[-1]} for blessing.",
+    f"Thanks to @{', @'.join(list(bless_config.roles.values())[:-1])} and @{list(bless_config.roles.values())[-1]} for blessing.",
     "60 second warning to leave the relay before shield bless",
     "```"
 ])
