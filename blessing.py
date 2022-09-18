4278c0595ee49340abd37ab9537d01a9aa2036be
cyroth <github@cyroth.com>
2022-09-18 22:16:42 +1000

    auto time for discord message

--- blessing.py
+++ blessing.py
@@ -2,17 +2,17 @@
 # Warframe blessing formater
 
 from configparser import ConfigParser
-import os
+import os, datetime
 config = ConfigParser()
 config.read("bless.ini")
+
+seperator = "============================================================================"
 
 # get values
 my_name = config.get('config', 'my_name')
 region = config.get('config', 'region')
-
 relay_name = config.get('bless_setup', 'relay_name')
 relay_instance = config.get('bless_setup', 'relay_instance')
-
 total_blessers = 0
 affinity_bless = config.get('roles', 'affinity_bless')
 if(len(affinity_bless) != 0):
@@ -33,16 +33,22 @@ shield_bless = config.get('roles', 'shield_bless')
 if(len(shield_bless) != 0):
     total_blessers += 1
 
-bless_types = ['affinity', 'credit', 'resource', 'damage', 'health', 'shield']
+# calculate time until bless
+delta = datetime.timedelta(hours=1)
+now = datetime.datetime.now()
+next_hour = (now + delta).replace(second=0, minute=0)
+wait_minutes = (next_hour - now).seconds/60
+wait_minutes = int(wait_minutes)
 
 print("```")
-print("========================================================")
+print(f"{seperator}")
 # output !bless command
-print(f"!Bless pc {region} {relay_name} {relay_instance} 15m ", end = "")
+bless_types = ['affinity', 'credit', 'resource', 'damage', 'health', 'shield']
+print(f"!bless pc {region} {relay_name} {relay_instance} {wait_minutes} min ", end = "")
 print(*bless_types[0:total_blessers])
 
 # output roles to paste into relay chat
-print("========================================================")
+print(f"{seperator}")
 print(f"BLESSING ROLES: @{affinity_bless} ---> {bless_types[0].capitalize()} | ", end = "")
 if total_blessers > 1:
     print(f"@{credit_bless} ---> {bless_types[1].capitalize()} | ", end = "")
@@ -54,20 +60,12 @@ if total_blessers > 4:
     print(f"@{health_bless} ---> {bless_types[4].capitalize()} | ", end = "")
 if total_blessers > 5:
     print(f"@{shield_bless} ---> {bless_types[5].capitalize()}  | ", end = "")
-
-# calculate and output time until bless
-import datetime
-delta = datetime.timedelta(hours=1)
-now = datetime.datetime.now()
-next_hour = (now + delta).replace(second=0, minute=0)
-wait_minutes = (next_hour - now).seconds/60
-wait_minutes = int(wait_minutes)
 print(f"Blessing in {wait_minutes} minutes", end = "")
 if total_blessers > 5:
-    print(f" | Shield bless will be at xx:01")
+    print(f" | Shield bless will be delayed by 1 minute")
 else:
     print()
-print("========================================================")
+print(f"{seperator}")
 
 # nag whisper for missing blessers and list for thanks
 nag_mesage = f"Reminder for bless at {relay_name.capitalize()} {relay_instance} in {region} region. You'll be on"
@@ -88,14 +86,14 @@ if total_blessers > 4:
 if total_blessers > 5:
     print(f"/w {shield_bless} {nag_mesage} {bless_types[5].capitalize()}")
     roll_call.append(shield_bless)
-print("========================================================")
+print(f"{seperator}")
 
 # roll call and thank you message
 roll_call.remove(my_name)
 print ("Roll call. @", end = "")
 print(' @'.join(roll_call), end = "")
 print(" :clem:")
-print("========================================================")
+print(f"{seperator}")
 print ("Thanks to ", end = "")
 print(', '.join(roll_call), end = "")
 print(" for blessing")
